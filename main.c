ff98f393c6d58e0e6f3cce5d1a748e69ca41e86c
mateidumitrescu <94167746+mateidumitrescu@users.noreply.github.com>
2022-04-15 18:46:02 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -14,10 +14,7 @@ int add_last(void **arr, int *len, data_structure *data);
 
 int add_at(void **arr, int *len, data_structure *data, int index);
 
-void find(void *data_block, int len, int index) 
-{
-
-}
+void find(void *data_block, int len, int index);
 
 int delete_at(void **arr, int *len, int index)
 {
@@ -70,6 +67,10 @@ int main() {
 				if (add_at(&arr, &len, data_block, index) != 0) {
 					return -1;
 				}
+			} else if (strcmp(token, "find") == 0) {
+				token = strtok(NULL, " ");
+				int index = atoi(token);
+				find(arr, len, index);
 			}
 		} else if (line[0] == 'p' || line[0] == 'e') {
 			if (strcmp(line, "print") == 0) {
@@ -118,9 +119,87 @@ int add_at(void **arr, int *len, data_structure *data, int index) {
 		return add_last(arr, len, data);
 	}
 
-	while (index > 0) {
-
+	unsigned int aux_len_1 = 0;
+	for (int i = 0; i < index; i++) {
+		unsigned char type = *(unsigned char *)(*arr + aux_len_1);
+		unsigned int len_of_data = *(unsigned int *)(*arr + sizeof(type) + aux_len_1);
+		aux_len_1 += sizeof(type) + sizeof(len_of_data) + len_of_data;
 	}
+
+	unsigned int aux_len_2 = aux_len_1;
+	for (int i = index - 1; i < *len; i++) {
+		unsigned char type = *(unsigned char *)(*arr + aux_len_2);
+		unsigned int len_of_data = *(unsigned int *)(*arr + sizeof(type) + aux_len_2);
+		aux_len_2 += sizeof(type) + sizeof(len_of_data) + len_of_data;
+	}
+
+	unsigned int new_data_len = sizeof(unsigned char) + sizeof(unsigned int) + data->header->len;
+	unsigned int new_arr_len = aux_len_2 + new_data_len;
+	*arr = realloc(*arr, new_arr_len);
+	void *aux_arr = malloc(aux_len_2 - aux_len_1 + new_data_len); // creating an auxiliar array to store after index data
+	DIE(!aux_arr, "Malloc failed\n");
+
+	memcpy(aux_arr, &data->header->type, sizeof(unsigned char));
+	memcpy(aux_arr + sizeof(unsigned char), &data->header->len, sizeof(unsigned int));
+	memcpy(aux_arr + sizeof(unsigned char) + sizeof(unsigned int), data->data, data->header->len);
+	memcpy(aux_arr + new_data_len, *arr + aux_len_1, aux_len_2 - aux_len_1);
+	memcpy(*arr + aux_len_1, aux_arr, new_data_len + aux_len_2 - aux_len_1);
+
+	(*len)++;
+	free(aux_arr);  // freeing auxiliar array
+	return 0;
+}
+
+void find(void *data_block, int len, int index) 
+{
+	if (index < 0 || index >= len) {
+		return;
+	}
+
+	unsigned char type;
+	unsigned int len_of_data;
+	unsigned int aux_len = 0;
+	for (int i = 0; i <= index; i++) {
+		type = *(unsigned char *)(data_block + aux_len);
+		len_of_data = *(unsigned int *)(data_block + sizeof(type));
+		aux_len += sizeof(unsigned char) + sizeof(len_of_data) + len_of_data;
+	}
+
+	aux_len -= len_of_data;  // getting back to the beginning of the elements to print
+	if (type == '1') {
+			printf("Tipul 1\n");
+			char *name_1 = (char *)(data_block + aux_len);
+			printf("%s pentru ", name_1);
+			int8_t sum_1 = *(int8_t *)(data_block + aux_len + strlen(name_1) + 1);
+			int8_t sum_2 = *(int8_t *)(data_block + aux_len + strlen(name_1) + 1 + sizeof(int8_t));
+			char *name_2 = (char *)(data_block + aux_len + strlen(name_1) + 1 + 2 * sizeof(int8_t));
+			printf("%s\n", name_2);
+			printf("%"PRId8"\n", sum_1);
+			printf("%"PRId8"\n", sum_2);
+			printf("\n");
+		} else if (type == '2') {
+			printf("Tipul 2\n");
+			char *name_1 = (char *)(data_block + aux_len);
+			printf("%s pentru ", name_1);
+			int16_t sum_1 = *(int16_t *)(data_block + aux_len + strlen(name_1) + 1);
+			int32_t sum_2 = *(int32_t *)(data_block + aux_len + strlen(name_1) + 1 + sizeof(int16_t));
+			char *name_2 = (char *)(data_block + aux_len + strlen(name_1) + 1 + sizeof(int16_t) + sizeof(int32_t));
+			printf("%s\n", name_2);
+			printf("%"PRId16"\n", sum_1);
+			printf("%"PRId32"\n", sum_2);
+			printf("\n");
+		} else {
+			printf("Tipul 3\n");
+			char *name_1 = (char *)(data_block + aux_len);
+			printf("%s pentru ", name_1);
+			int32_t sum_1 = *(int32_t *)(data_block + aux_len + strlen(name_1) + 1);
+			int32_t sum_2 = *(int32_t *)(data_block + aux_len + strlen(name_1) + 1 + sizeof(int32_t));
+			char *name_2 = (char *)(data_block + aux_len + strlen(name_1) + 1 + 2 * sizeof(int32_t));
+			printf("%s\n", name_2);
+			printf("%"PRId32"\n", sum_1);
+			printf("%"PRId32"\n", sum_2);
+			printf("\n");
+		}
 }
 
 void create_data(char *line, data_structure *data_block, char *token) {
